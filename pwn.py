b759def0e2aa415d0ccbc0f38f4aebc13c3767a8
exploit3dguy <51859413+exploit3dguy@users.noreply.github.com>
2021-10-08 18:59:56 +0200

    Add files via upload

--- pwn.py
+++ pwn.py
@@ -8,12 +8,11 @@ import usbexec
 
 dev = usbexec.PwnedUSBDevice()
 
+dev.write_memory_ptr(0x1800C2F58, 0x1800CEC00)
+
+dev.write_memory(0x1800CEC00, open('bin/rompatch.bin').read())
 
 
-dev.write_memory_ptr(0x1800C2F58, 0x10000078C)
-
-
-dev.write_memory(0x180087870, '\x01')
 
 
 device = dfu.acquire_device()
@@ -24,5 +23,5 @@ dfu.usb_reset(device)
 dfu.release_device(device)
 
 
-print 'You can now load unpacked iBSS'
+print 'patched mapping and signature checks'
 
